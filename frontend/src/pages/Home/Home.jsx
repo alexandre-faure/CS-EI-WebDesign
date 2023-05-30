c69d1b90f2c9d7fbf59472a12887b76e80d12afa
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-05-30 16:48:47 +0200

    Updated Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import FilterSide from '../../components/FilterSide/FilterSide';
 import './Home.css';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://react.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home-container">
+      <FilterSide />
+      <div></div>
     </div>
   );
 }
