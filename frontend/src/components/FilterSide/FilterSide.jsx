7a42366377ba6d48ef7a6f24bff5f1467a7fb50f
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-05-31 17:03:31 +0200

    Added Reducer and Context

--- frontend/src/components/FilterSide/FilterSide.jsx
+++ frontend/src/components/FilterSide/FilterSide.jsx
@@ -1,16 +1,42 @@
 import './FilterSide.css';
+import { useEffect, useReducer } from 'react';
 import GenreCheckbox from '../GenreCheckbox/GenreCheckbox';
+import { HomeContext, HomeDispatchContext } from '../../contexts/HomeContext';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'toggleGenre':
+      var newState = JSON.parse(JSON.stringify(state));
+      if (newState.genres[action.payload.genreId]) {
+        delete newState.genres[action.payload.genreId];
+      } else {
+        newState.genres[action.payload.genreId] = true;
+      }
+
+      return newState;
+
+    default:
+      return state;
+  }
+}
 
 function FilterSide() {
-  return (
-    <div className="filterside-container">
-      <div className="filterside-h1">Filtrer :</div>
-      <div className="filterside-h2">Genre</div>
-      <GenreCheckbox genreId="action" genreName="Action" />
-      <GenreCheckbox genreId="aventure" genreName="Aventure" />
+  const [state, dispatch] = useReducer(reducer, { genres: {} });
 
-      <div className="filterside-h2">Date</div>
-    </div>
+  return (
+    <HomeContext.Provider value={state}>
+      <HomeDispatchContext.Provider value={dispatch}>
+        <div className="filterside-container">
+          <div className="filterside-h1">Filtrer</div>
+          <div className="filterside-h2">Genre</div>
+          <div className="filterside-genre-list-container">
+            <GenreCheckbox genreId="action" genreName="Action" />
+            <GenreCheckbox genreId="aventure" genreName="Aventure" />
+          </div>
+          <div className="filterside-h2">Date</div>
+        </div>
+      </HomeDispatchContext.Provider>
+    </HomeContext.Provider>
   );
 }
 
--- frontend/src/components/GenreCheckbox/GenreCheckbox.jsx
+++ frontend/src/components/GenreCheckbox/GenreCheckbox.jsx
@@ -1,16 +1,35 @@
 import './GenreCheckbox.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-i  ';
+import { faSquare } from '@fortawesome/free-solid-svg-icons';
+import { useContext, useEffect } from 'react';
+import $ from 'jquery';
+import { HomeContext, HomeDispatchContext } from '../../contexts/HomeContext';
 
 function GenreCheckbox(data) {
+  var boxIcon;
+  const state = useContext(HomeContext);
+  const dispatch = useContext(HomeDispatchContext);
+
+  function handleCheckboxClick() {
+    dispatch({
+      type: 'toggleGenre',
+      payload: { genreId: data.genreId },
+    });
+  }
+
+  boxIcon = data.genreId in state.genres ? faSquareCheck : faSquare;
+
   return (
     <div className="filterside-genre">
       <label>
-        <input
-          type="checkbox"
+        <FontAwesomeIcon
+          icon={boxIcon}
           id={data.genreId}
-          name={data.genreId}
           className="filterside-genre-checkbox"
+          onClick={handleCheckboxClick}
         />
-        {data.genreName}
+        <div className="filterside-genre-title">{data.genreName}</div>
       </label>
     </div>
   );
