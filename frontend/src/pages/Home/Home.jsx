3167d07799e2f4b1faa73992d2211807271f6681
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-05-31 14:14:08 +0200

    Homepage update

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import FilterSide from '../../components/FilterSide/FilterSide';
+import MovieSide from '../../components/MovieSide/MovieSide';
+import SearchBar from '../../components/SearchBar/SearchBar';
+
 import './Home.css';
 
 function Home() {
   return (
     <div className="home-container">
+      <SearchBar />
       <FilterSide />
+      <MovieSide />
     </div>
   );
 }
