cd23305169844c2a8b27a76baf2123817362ed5d
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-05-31 20:22:51 +0200

    Moved Context to Home level

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,16 +1,46 @@
+import { useReducer } from 'react';
 import FilterSide from '../../components/FilterSide/FilterSide';
 import MovieSide from '../../components/MovieSide/MovieSide';
 import SearchBar from '../../components/SearchBar/SearchBar';
 
+import { HomeContext } from '../../contexts/HomeContext';
+import { HomeDispatchContext } from '../../contexts/HomeContext';
+
 import './Home.css';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'toggleGenre':
+      var newState = JSON.parse(JSON.stringify(state));
+      if (newState.genres[action.payload.genreId]) {
+        delete newState.genres[action.payload.genreId];
+      } else {
+        newState.genres[action.payload.genreId] = true;
+      }
+
+      return newState;
+
+    default:
+      return state;
+  }
+}
+
 function Home() {
+  const [state, dispatch] = useReducer(reducer, {
+    genres: {},
+    highlightedMovie: {},
+  });
+
   return (
-    <div className="home-container">
-      <SearchBar />
-      <FilterSide />
-      <MovieSide />
-    </div>
+    <HomeContext.Provider value={state}>
+      <HomeDispatchContext.Provider value={dispatch}>
+        <div className="home-container">
+          <SearchBar />
+          <FilterSide />
+          <MovieSide />
+        </div>
+      </HomeDispatchContext.Provider>
+    </HomeContext.Provider>
   );
 }
 
