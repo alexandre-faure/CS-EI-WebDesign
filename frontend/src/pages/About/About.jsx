a7db458868cec6a1ec987713778dd0336a968a6f
Bornet Apolline <apolline.bornet@student-cs.fr>
2023-06-02 11:05:55 +0200

    mise en page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,7 +1,17 @@
 import './About.css';
 
 function About() {
-  return <div className="about">about page</div>;
+  return <div className="about">
+   <p> Créé par Alexandre Faure, Apolline Bornet et Nicolas Descamps dans le cadre d’un projet académique de Centralesupélec, CineSuggest est un site internet de recommandation de films. Avec son système d’identification, 
+    il permet de se connecter et de recevoir des recommandations selon ses critères de tri et de filtrage.  
+    Il comporte 5 pages principales : <br></br>
+  -	La page d’authentification<br></br>
+  -	La page de création de compte<br></br>
+  -	La page d’accueil où apparaissent les recommandations<br></br>
+  -	La page profil, où l’on peut modifier ses informations (nom, prénom, pseudo, critères par défaut…)<br></br>
+  -	La page Mes listes, où l’on peut retrouver la liste des films déjà vu et des films à voir. <br></br>
+  </p>
+  </div>;
 }
 
 export default About;
