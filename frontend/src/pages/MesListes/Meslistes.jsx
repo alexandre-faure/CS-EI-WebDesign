34097fe3ccf06d048ef47a135f89be0873065cdf
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2023-06-02 09:25:31 +0200

    Home update

--- frontend/src/pages/MesListes/Meslistes.jsx
+++ frontend/src/pages/MesListes/Meslistes.jsx
@@ -1,11 +1,21 @@
+import { useContext } from 'react';
+import MovieSlider from '../../components/MovieSlider/MovieSlider';
 import './Meslistes.css';
+import { HomeContext } from '../../contexts/HomeContext';
 
 function Meslistes() {
-    return (
-        <div>
-            <h1 className = 'h1ml' >Metz c'est mieux</h1>
-        </div>
-      );   
+  const state = useContext(HomeContext);
+
+  return (
+    <div className="mes-listes-container">
+      <div className="mes-listes-h1">Mes listes</div>
+      <MovieSlider
+        slider_id={'to-see'}
+        title={'A voir'}
+        movieList={state.homeSliders[0].movies}
+      />
+    </div>
+  );
 }
-        
+
 export default Meslistes;
