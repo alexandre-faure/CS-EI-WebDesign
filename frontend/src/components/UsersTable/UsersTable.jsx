9822d0064f8c33568959e42b6309675dd6f5d1e6
Alexandre Faure <alexandre.faure@student-cs.fr>
2023-06-02 09:44:23 +0200

    few updates

--- frontend/src/components/UsersTable/UsersTable.jsx
+++ frontend/src/components/UsersTable/UsersTable.jsx
@@ -17,7 +17,6 @@ function UsersTable({ users, onSuccessfulUserDeletion }) {
             <th>Prénom</th>
             <th>Nom de Famille</th>
             <th>Date de Naissance</th>
-            <th>Mot de passe</th>
             <th>Supprimer</th>
           </tr>
         </thead>
@@ -28,9 +27,8 @@ function UsersTable({ users, onSuccessfulUserDeletion }) {
             <td>{user.user_firstname}</td>
             <td>{user.user_lastname}</td>
             <td>{user.user_date_of_birth}</td>
-            <td>{user.user_password}</td>
               <td>
-                <button onClick={() => deleteUser(user.user_id)}>x</button>
+                <button className="delete-button" onClick={() => deleteUser(user.user_id)}>x</button>
               </td>
             </tr>
           ))}
